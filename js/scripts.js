3e65c352b5eab2314615a702fe977bb90c78aa23
versaluxperu-create <versaluxperu@gmail.com>
2025-10-30 22:14:00 -0500

    Actualizacion

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@
 window.addEventListener('load', () => {
   setTimeout(() => document.getElementById('preloader').classList.add('hide'), 280);
 });
+
+// Navbar: efecto “scrolled”
+(function(){
+  const nav = document.querySelector('.navbar');
+  const onScroll = () => {
+    if (window.scrollY > 10) nav.classList.add('nav-scrolled');
+    else nav.classList.remove('nav-scrolled');
+  };
+  onScroll();
+  window.addEventListener('scroll', onScroll, { passive: true });
+})();
